03a3e14e58d2fcafa7b7f7451b66bcf06fc5da48
Tatu <tatutequabo@gmail.com>
2025-12-25 19:26:57 -0500

    Serve static front-end files with Express

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 
-// Dynamic port for Render
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from public folder
+// Serve static files from "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Handle root route
+// Serve index.html on root
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Habesha Bingo running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+
 
 
 
