c601ec6a182733b5d57f0e5a334aa3c661c13810
Tatu <tatutequabo@gmail.com>
2026-01-01 21:18:24 -0500

    Fix Render crash: remove firebase browser SDK

--- index.js
+++ index.js
@@ -3,40 +3,37 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const path = require("path");
-const { db } = require("./firebase");
-const { ref, set } = require("firebase/database");
 
-// Serve static files
 app.use(express.static(path.join(__dirname)));
 
-io.on("connection", (socket) => {
-  console.log("New user connected");
+let players = {};
 
-  // Player joins
-  socket.on("playerJoin", async (name) => {
-    await set(ref(db, "players/" + socket.id), { name, joinedAt: Date.now() });
-    io.emit("updatePlayers", { id: socket.id, name });
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("playerJoin", (name) => {
+    players[socket.id] = name;
+    io.emit("playersUpdate", players);
   });
 
-  // Number called by host
   socket.on("numberCalled", (number) => {
     io.emit("numberCalled", number);
   });
 
-  // Player wins
-  socket.on("bingo", async (playerName) => {
-    io.emit("bingoWin", playerName);
-    await set(ref(db, "winners/" + Date.now()), { player: playerName });
+  socket.on("bingo", (name) => {
+    io.emit("bingoWin", name);
   });
 
-  // Disconnect
-  socket.on("disconnect", async () => {
-    await set(ref(db, "players/" + socket.id), null);
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("playersUpdate", players);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Habesha Bingo running on port ${PORT}`));
+http.listen(PORT, () => {
+  console.log("Habesha Bingo running on port", PORT);
+});
 
 
 
